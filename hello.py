0f8f89d61012b65b2e3c194fdd9cea5f758a39dd
ashishkx <ashishkx@gmail.com>
2015-12-29 19:46:01 +0530

    Testing out Flask

--- hello.py
+++ hello.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
 	return 'Hello World!'
+	
+@app.route('/<user>')
+def hello_world2(user):
+	return 'Hello World %s! <h1 color = red> This is html right here </h1> %s' % (user, request.method)
+	
+@app.route('/index/')
+def index():
+		return render_template('index.html')
+
 	
 if __name__ == '__main__':
 	app.run()
