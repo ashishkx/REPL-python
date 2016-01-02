b43213783916d0f8c41722dc6e207954f6dda887
ashishkx <ashishkx@gmail.com>
2016-01-02 21:06:19 +0530

    Removed repeated render_template

--- hello.py
+++ hello.py
@@ -4,18 +4,17 @@ app = Flask(__name__)
 		
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	if request.method == 'POST':
-		text = request.form['userinput']
-		new_text = '%s\n'%text + text.upper()
-		return render_template('index.html', userout=new_text)
-	
-	
-	else:	
-		return render_template('index.html')
+    new_text=""
+    if request.method == 'POST':
+        text = request.form['userinput']
+        new_text = '%s\n'%text + text.upper()
+
+    return render_template('index.html', userout=new_text)
+
 	
 	
 
 	
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
