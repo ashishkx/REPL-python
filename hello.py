50207563acf77830fcaa50dd1fce9ebb59f63f92
ashishkx <ashishkx@gmail.com>
2016-01-05 10:49:03 +0530

    added subprocess module

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, render_template
-import os
+import subprocess
 app = Flask(__name__)
 
 
-makefile(user_code):
-	f = open("textarea_input.py", "w+")
-	f.write(user_code)
-	f.close()
+def makefile(user_code):
+    f = open("textarea_input.py", "w+")
+    f.write(user_code)
+    f.close()
 		
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -14,5 +14,8 @@ def index():
     if request.method == 'POST':
         text = request.form['userinput']
         makefile(text)
-		os.system("python textarea_input.py")
-    return render_template('index.html', userout=)
+        proc = subprocess.Popen(["textarea_input.py"])
+    return render_template('index.html', userout=None)
+    
+if __name__ == '__main__':
+    app.run()
